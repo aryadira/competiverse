8cc7fec7e6b777f5206f316738579a58a116c219
Adiptra <adiirawan2705@gmail.com>
2023-07-28 04:36:40 -0700

    Beranda update

--- src/pages/Beranda.jsx
+++ src/pages/Beranda.jsx
@@ -1,19 +1,36 @@
 import { Button, } from "../components";
 
 const Home = {
-  section: "transparent dark:bg-gray-900   flex justify-center",
-  container: "py-8 px-4 max-w-screen-xl lg:py-16 gap-8 lg:gap-16 text-center w-[700px]",
-  heading: "mb-4 text-5xl font-bold tracking-tight leading-none text-gray-900 md:text-5xl lg:text-4xl dark:text-light",
+  baseColor: "bg-gradient-to-r from-purple-600 to-pink-500 bg-clip-text text-transparent",
+  section: {
+    defaultSection: "transparent dark:bg-gray-900",
+    justify: "flex justify-center",
+  },
+  container: {
+    defaultContainer: "py-8 px-4 max-w-screen-xl lg:py-16",
+    gap: "gap-8 lg:gap-16",
+    alignText: "text-center",
+    width: "w-[700px]"
+  },
+  heading: {
+    defaultHeading: "mb-4 tracking-tight leading-none text-gray-900 dark:text-light",
+    fontWeightBold: "font-bold",
+    fontWeightExtrabold: "font-extrabold",
+    fontSize1: "text-5xl md:text-5xl lg:text-4xl",
+    fontSize2: "text-xl",
+    width:"w-[300px]"
+  },
   content: "flex flex-col justify-center",
   paragraph: "mb-8 text-lg font-normal text-gray-500 lg:text-xl dark:text-gray-400"
 };
 
 const Beranda = () => {
   return (
-    <section id="berandaHeader" className={Home.section}>
-      <div className={Home.container}>
+    <>
+    <section id="berandaHeader" className={`${Home.section.defaultSection} ${Home.section.justify}`}  >
+      <div className={`${Home.container.defaultContainer} ${Home.container.gap} ${Home.container.alignText}  ${Home.container.width}`}>
         <div className={Home.content}>
-          <h1 className={Home.heading}>Temukan. Cari Lomba yang Anda Minati!!</h1>
+          <h1 className={`${Home.heading.defaultHeading} ${Home.heading.fontWeightBold} ${Home.heading.fontSize1}`}>Temukan. Cari Lomba yang Anda Minati!!</h1>
           <p className={Home.paragraph}>
             Temukan lomba-lomba menarik sesuai minat dan bakat Anda. Raih prestasi gemilang dalam passion Anda. Bergabunglah sekarang.
           </p>
@@ -23,6 +40,12 @@ const Beranda = () => {
         </div>
       </div>
     </section>
+    <section id="berandaCard" className={Home.section.defaultSection}>
+      <div className={Home.container.defaultContainer}>
+        <h1 className={`${Home.heading.defaultHeading} ${Home.heading.fontWeightExtrabold} ${Home.heading.fontSize2} ${Home.baseColor} ${Home.heading.width}`}>Rekomendasi Untuk Anda</h1>
+      </div>
+    </section>
+    </>
   );
 };
 
