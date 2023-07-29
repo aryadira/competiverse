ed392ee7fcc10ceb5a101744b24acbc3104f6200
Adiptra <adiirawan2705@gmail.com>
2023-07-28 20:08:11 -0700

    Landing Update

--- src/pages/LandingPage.jsx
+++ src/pages/LandingPage.jsx
@@ -3,7 +3,7 @@ import { Jumbotron } from "../components";
 const LandingPage = () => {
   return (
     <div>
-      <Jumbotron />
+      <Jumbotron heading="Lomba? Competiverse Solusinya!" paragraph="Dengan kehadiran Competiverse, mencari lomba tidak akan pernah menjadi masalah lagi. Platform ini menyediakan beragam lomba menarik dari berbagai penyelenggara. Kemudahan dan efisiensinya membuat proses pencarian lomba menjadi jauh lebih praktis dan efektif."/>
     </div>
   );
 };
