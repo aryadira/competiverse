e9994b1df128574ae406424fcc92d2ebb38e054d
aryadira <aryadyra27@gmail.com>
2023-07-31 14:40:22 +0700

    melengkapi landing page

--- src/pages/LandingPage.jsx
+++ src/pages/LandingPage.jsx
@@ -136,11 +136,7 @@ const LandingPage = () => {
                 <span className='text-base font-normal leading-tight text-gray-500'>24×7 phone & email support</span>
               </li>
             </ul>
-            <button
-              type='button'
-              className='text-white bg-blue-600 hover:bg-blue-700 focus:ring-4 focus:outline-none focus:ring-blue-200 dark:focus:ring-blue-900 font-medium rounded-lg text-sm px-5 py-2.5 inline-flex justify-center w-full text-center'>
-              Choose plan
-            </button>
+            <Button className={`block w-full`}>Choose Plan</Button>
           </div>
           <div className='w-full max-w-sm p-4 bg-white border border-gray-200 rounded-lg shadow sm:p-8 dark:bg-gray-800 dark:border-gray-700'>
             <h5 className='mb-4 text-xl font-medium text-gray-500 dark:text-gray-400'>Standard plan</h5>
@@ -228,11 +224,7 @@ const LandingPage = () => {
                 <span className='text-base font-normal leading-tight text-gray-500'>24×7 phone & email support</span>
               </li>
             </ul>
-            <button
-              type='button'
-              className='text-white bg-blue-600 hover:bg-blue-700 focus:ring-4 focus:outline-none focus:ring-blue-200 dark:focus:ring-blue-900 font-medium rounded-lg text-sm px-5 py-2.5 inline-flex justify-center w-full text-center'>
-              Choose plan
-            </button>
+            <Button className={`block w-full`}>Choose Plan</Button>
           </div>
           <div className='w-full max-w-sm p-4 bg-white border border-gray-200 rounded-lg shadow sm:p-8 dark:bg-gray-800 dark:border-gray-700'>
             <h5 className='mb-4 text-xl font-medium text-gray-500 dark:text-gray-400'>Standard plan</h5>
@@ -320,11 +312,7 @@ const LandingPage = () => {
                 <span className='text-base font-normal leading-tight text-gray-500'>24×7 phone & email support</span>
               </li>
             </ul>
-            <button
-              type='button'
-              className='text-white bg-blue-600 hover:bg-blue-700 focus:ring-4 focus:outline-none focus:ring-blue-200 dark:focus:ring-blue-900 font-medium rounded-lg text-sm px-5 py-2.5 inline-flex justify-center w-full text-center'>
-              Choose plan
-            </button>
+            <Button className={`block w-full`}>Choose Plan</Button>
           </div>
           <div className='w-full max-w-sm p-4 bg-white border border-gray-200 rounded-lg shadow sm:p-8 dark:bg-gray-800 dark:border-gray-700'>
             <h5 className='mb-4 text-xl font-medium text-gray-500 dark:text-gray-400'>Standard plan</h5>
@@ -412,11 +400,7 @@ const LandingPage = () => {
                 <span className='text-base font-normal leading-tight text-gray-500'>24×7 phone & email support</span>
               </li>
             </ul>
-            <button
-              type='button'
-              className='text-white bg-blue-600 hover:bg-blue-700 focus:ring-4 focus:outline-none focus:ring-blue-200 dark:focus:ring-blue-900 font-medium rounded-lg text-sm px-5 py-2.5 inline-flex justify-center w-full text-center'>
-              Choose plan
-            </button>
+            <Button className={`block w-full`}>Choose Plan</Button>
           </div>
         </div>
       </section>
