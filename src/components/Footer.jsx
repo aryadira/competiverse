324c1bf1cad90a88de0062f8040e244c550d57ca
aryadira <aryadyra27@gmail.com>
2023-08-07 21:06:28 +0700

    add calender

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -48,7 +48,7 @@ const Footer = () => {
           <div className='sm:flex sm:items-center sm:justify-between'>
             <span className='text-sm text-gray-500 sm:text-center dark:text-gray-400'>
               © 2023{" "}
-              <a href='https://flowbite.com/' className='hover:underline'>
+              <a href='/' className='hover:underline'>
                 Competiverse™
               </a>
               . All Rights Reserved.
